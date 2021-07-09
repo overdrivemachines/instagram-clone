a7d74dcdb94d227cacb45e8e25595079255824f8
Dipen Chauhan <get.dipen@gmail.com>
2021-07-08 19:08:18 -0700

    Added accounts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'public/homepage'
   devise_for :accounts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/dashboard", to: "accounts#index"
   root to: "public#homepage"
 end
