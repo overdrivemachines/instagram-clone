f1209a64279f524cf58a8f83a2378ba914190e88
Dipen Chauhan <get.dipen@gmail.com>
2021-07-10 10:51:48 -0700

    Added posts controller with new, create and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: "public#homepage"
   get 'public/homepage'
   devise_for :accounts
   get "/dashboard", to: "accounts#index"
-  root to: "public#homepage"
+  resources :posts, only: [:new, :create, :show]
 end
