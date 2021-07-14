1d4affc3aa36ffd7155c367fc7891cc0426fe398
Dipen Chauhan <get.dipen@gmail.com>
2021-07-14 01:01:01 -0700

    Annotated

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  image      :string
+#  active     :boolean
+#  account_id :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
   # Connect model to uploader file
   mount_uploader :image, ImageUploader
