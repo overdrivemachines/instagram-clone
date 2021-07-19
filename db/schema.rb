fee8f6175ad10766387036eebd45834eb67cbce6
Dipen Chauhan <get.dipen@gmail.com>
2021-07-18 23:59:05 -0700

    Added post description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_103132) do
+ActiveRecord::Schema.define(version: 2021_07_19_064436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_07_18_103132) do
     t.bigint "account_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["account_id"], name: "index_posts_on_account_id"
   end
 
