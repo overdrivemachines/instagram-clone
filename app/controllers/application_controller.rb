92a872a9d39cfb803ee22c17beae2d76a4836eb0
Dipen Chauhan <get.dipen@gmail.com>
2021-07-18 17:26:18 -0700

    Displaying account image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # Allow username, first_name and last_name
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :email, :description, :website, :image, :image_cache])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :email, :description, :website, :image, :image_cache])
   end
 
 end
