5350360d8bed4698de04ed592810bfb23a4f807c
Dipen Chauhan <get.dipen@gmail.com>
2021-07-19 21:10:40 -0700

    Created likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_064436) do
+ActiveRecord::Schema.define(version: 2021_07_20_040939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 2021_07_19_064436) do
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_likes_on_account_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "image"
     t.boolean "active"
@@ -48,4 +57,6 @@ ActiveRecord::Schema.define(version: 2021_07_19_064436) do
     t.index ["account_id"], name: "index_posts_on_account_id"
   end
 
+  add_foreign_key "likes", "accounts"
+  add_foreign_key "likes", "posts"
 end
