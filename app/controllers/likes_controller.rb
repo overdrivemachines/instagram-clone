2301634ce0653557fbcd0f687e7f1b5df344c6f7
Dipen Chauhan <get.dipen@gmail.com>
2021-07-24 04:58:19 -0700

    Respond with js

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,15 @@
 class LikesController < ApplicationController
   def save_like
     @like = Like.new(post_id: params[:post_id], account_id: current_account.id)
-
+    @post_id = params[:post_id]
     respond_to do |format|
-      format.json {
+      format.js {
         if @like.save
-          {success: true}
+          @success = true
         else
-          {success: false}
+          success = false
         end
+        render "posts/like"
       }
     end
   end
